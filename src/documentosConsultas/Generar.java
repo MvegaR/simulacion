21b8600b875ff566fad682e52e7108109103f344
MvegaR <mvegar@alumnos.ubiobio.cl>
2017-04-20 00:26:45 -0300

    texto

--- src/documentosConsultas/Generar.java
+++ src/documentosConsultas/Generar.java
@@ -13,25 +13,29 @@ public class Generar {
 	public static void main(String[] args) {
 		File documentosFIle = new File("files/cacm.all");
 		File consultasFile = new File("files/query.text");
+		File palabrasComunesFile = new File("files/common_words");
+		
 		ArrayList<Documento> documentos = new ArrayList<>();
 		ArrayList<Consulta> consultas = new ArrayList<>();
+		ArrayList<String> palabrasComunes = new ArrayList<>();
+		
 		Documento.generarDocumentos(documentosFIle, documentos);
 		Consulta.generarConsultas(consultasFile, consultas);
+		
+		
 		System.out.println("Documentos: "+documentos.size());
-		System.err.println("Consultas:"+consultas.size());
+		System.out.println("Consultas:"+consultas.size());
 		/*//Para Imprimir todos los documentos, agregar un '/' al inicio de esta linea para descomentar codigo.
 		for(Documento d: documentos){
 			System.out.println(d);
 		}
 		//*/
+		
 		/*//Para imprimir todas las consultas, agregar un '/' al inicio de esta linea para descomentar codigo.
 		for(Consulta c: consultas){
 			System.out.println(c);
 		}
 		//*/
-		
-		
-		
 		
 	}
 	
@@ -40,6 +44,8 @@ public class Generar {
 		
 		return null;
 	}
+	
+	
 	
 	
 	
