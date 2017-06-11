365d1939ed84702da697257a1ee3db66cfa46a89
MvegaR <mvegar@alumnos.ubiobio.cl>
2017-06-10 23:05:33 -0400

    docs

--- src/analisis/Precision.java
+++ src/analisis/Precision.java
@@ -13,39 +13,78 @@ public class Precision {
 	Double precision;
 	/** Cantidad de docuemntos relevantes para la consulta */
 	Integer documentosRelevantes;
-	/** Numero de documentos maximos a desplegar y a incluir en el calculo de precisión */
+	/** Número de documentos maximos a desplegar y a incluir en el calculo de precisión */
 	Integer pIn;
-	
+	/**
+	 * Constructor clase Precision
+	 * @param idConsulta Identificador de la consulta
+	 * @param precision precision de la consulta
+	 * @param documentosRelevantes cantidad de docuemntos relevantes en la consulta
+	 * @param pIn Número de documentos maximos a desplegar y a incluir en el calculo de precisión 
+	 */
 	public Precision(Integer idConsulta, Double precision, Integer documentosRelevantes, Integer pIn) {
 		this.idConsulta = idConsulta;
 		this.precision = precision;
 		this.documentosRelevantes = documentosRelevantes;
 		this.pIn = pIn;
 	}
+	/** 
+	 * Entrega el identificador de la consulta
+	 * @return Identificador valor entero
+	 */
 	public Integer getIdConsulta() {
 		return idConsulta;
 	}
+	/** 
+	 * Modifica el identificador de la consulta 
+	 * @param idConsulta nuevo identificador
+	 */
 	public void setIdConsulta(Integer idConsulta) {
 		this.idConsulta = idConsulta;
 	}
+	/**
+	 * Entrega la precisión
+	 * @return Precisión, valor Real.
+	 */
 	public Double getPrecision() {
 		return precision;
 	}
+	/**
+	 * Cambia el valor de la precisión
+	 * @param precision nuevo valor
+	 */
 	public void setPrecision(Double precision) {
 		this.precision = precision;
 	}
+	/**
+	 * Entrega la cantidad de documentos relevantes
+	 * @return cantidad de documentos relevantes
+	 */
 	public Integer getDocumentosRelevantes() {
 		return documentosRelevantes;
 	}
+	/**
+	 * Modifica el valor de la cantidad de documentos relevantes
+	 * @param documentosRelevantes
+	 */
 	public void setDocumentosRelevantes(Integer documentosRelevantes) {
 		this.documentosRelevantes = documentosRelevantes;
 	}
+	/**
+	 * Entrega el valor PIn utilizado
+	 * @return valor Pin
+	 */
 	public Integer getpIn() {
 		return pIn;
 	}
+	/**
+	 * Modifica el valor pIn
+	 * @param pIn nuevo valor
+	 */
 	public void setpIn(Integer pIn) {
 		this.pIn = pIn;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -56,6 +95,7 @@ public class Precision {
 		result = prime * result + ((precision == null) ? 0 : precision.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
