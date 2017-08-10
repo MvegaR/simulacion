8c795f618998195c54ebb76b553433d2d2f98e4b
MvegaR <mvegar@alumnos.ubiobio.cl>
2017-08-10 00:19:55 -0400

    funcion?

---
+++ src/modelo/ProbabilisticInterval.java
@@ -0,0 +1,101 @@
+package modelo;
+
+public class ProbabilisticInterval {
+	
+	private Double min;
+	private Double max;
+	
+	public ProbabilisticInterval(Double min, Double max) {
+		super();
+		this.min = min;
+		this.max = max;
+		
+	}
+
+	
+	/**
+	 * @return the min
+	 */
+	public Double getMin() {
+		return min;
+	}
+
+
+
+	/**
+	 * @param min the min to set
+	 */
+	public void setMin(Double min) {
+		this.min = min;
+	}
+
+
+
+	/**
+	 * @return the max
+	 */
+	public Double getMax() {
+		return max;
+	}
+
+
+
+	/**
+	 * @param max the max to set
+	 */
+	public void setMax(Double max) {
+		this.max = max;
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((max == null) ? 0 : max.hashCode());
+		result = prime * result + ((min == null) ? 0 : min.hashCode());
+		return result;
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof ProbabilisticInterval))
+			return false;
+		ProbabilisticInterval other = (ProbabilisticInterval) obj;
+		if (max == null) {
+			if (other.max != null)
+				return false;
+		} else if (!max.equals(other.max))
+			return false;
+		if (min == null) {
+			if (other.min != null)
+				return false;
+		} else if (!min.equals(other.min))
+			return false;
+		return true;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "ProbabilisticInterval [min=" + min + ", max=" + max + "]";
+	}
+	
+	
+	
+
+}
