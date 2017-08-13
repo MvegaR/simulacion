8f33f1f6d2256816f89dbd7f0f6687ce9cd254dd
MvegaR <mvegar@alumnos.ubiobio.cl>
2017-08-13 01:04:32 -0300

    resp

--- src/controlador/VentanaController.java
+++ src/controlador/VentanaController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
@@ -12,16 +13,16 @@ import javafx.scene.layout.BorderPane;
 
 
 public class VentanaController implements Initializable{
-	@FXML
-	private TreeView<String> tree;
-	
-	@FXML
-	private BorderPane panelContenido;
-	
-	@FXML
-	private TableView<String> tablaDatos;
 	
 	private final String[] dataSets = {"CACM", "MED", "CRAN", "CISI", "LISA", "ADI", "TIME", "ISWC2015"};
+	@FXML
+	private TreeView<String> tree;
+	@FXML
+	private BorderPane panelContenido;
+	@FXML
+	private TableView<String> tablaDatos;
+	@FXML
+	private Label dataSetName;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -31,5 +32,72 @@ public class VentanaController implements Initializable{
 
 		
 	}
+	
+	/**
+	 * @return the tree
+	 */
+	public TreeView<String> getTree() {
+		return tree;
+	}
+
+	/**
+	 * @param tree the tree to set
+	 */
+	public void setTree(TreeView<String> tree) {
+		this.tree = tree;
+	}
+
+	/**
+	 * @return the panelContenido
+	 */
+	public BorderPane getPanelContenido() {
+		return panelContenido;
+	}
+
+	/**
+	 * @param panelContenido the panelContenido to set
+	 */
+	public void setPanelContenido(BorderPane panelContenido) {
+		this.panelContenido = panelContenido;
+	}
+
+	/**
+	 * @return the tablaDatos
+	 */
+	public TableView<String> getTablaDatos() {
+		return tablaDatos;
+	}
+
+	/**
+	 * @param tablaDatos the tablaDatos to set
+	 */
+	public void setTablaDatos(TableView<String> tablaDatos) {
+		this.tablaDatos = tablaDatos;
+	}
+
+	/**
+	 * @return the dataSetName
+	 */
+	public Label getDataSetName() {
+		return dataSetName;
+	}
+
+	/**
+	 * @param dataSetName the dataSetName to set
+	 */
+	public void setDataSetName(Label dataSetName) {
+		this.dataSetName = dataSetName;
+	}
+
+	/**
+	 * @return the dataSets
+	 */
+	public String[] getDataSets() {
+		return dataSets;
+	}
+
+	
+	
+
 
 }
