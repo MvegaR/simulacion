e4933451b85d3d34de48e900644fc89205f0923d
MvegaR <mvegar@alumnos.ubiobio.cl>
2017-08-12 20:51:16 -0400

    ventana

---
+++ src/controlador/VentanaController.java
@@ -0,0 +1,18 @@
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class VentanaController implements Initializable{
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test!!!!!!!!!");
+		
+	}
+
+}
---
+++ src/vista/Ventana.java
@@ -0,0 +1,38 @@
+package vista;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.print.DocFlavor.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Ventana extends Application {
+
+	private final String[] dataSets = {"CACM", "MED", "CRAN", "CISI", "LISA", "ADI", "TIME", "ISWC2015"};
+
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage windows) throws IOException {
+		windows.setTitle("");
+	     try {
+	    	 Parent root = FXMLLoader.load(getClass().getResource("ventana.fxml"));
+	         Scene scene = new Scene(root);
+			 windows.setScene(scene);
+			 windows.show();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+}
