8da9d7a6c156dccecd993cbde6a476d4935c77b4
MvegaR <mvegar@alumnos.ubiobio.cl>
2017-08-13 23:56:59 -0300

    REsplado

--- src/controlador/VentanaController.java
+++ src/controlador/VentanaController.java
@@ -12,6 +12,9 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
@@ -37,9 +40,41 @@ public class VentanaController implements Initializable{
 	private ProgressBar cargaLecturaDataSet;
 	@FXML
 	private Button buttonLeerDataSet;
-	
+	@FXML
+	private Label labelCantidadDocumentos;
+	@FXML
+	private Label labelCantidadConsultas;
+	@FXML
+	private Label labelPalabrasTotalesComunes;
+	@FXML
+	private Label labelPalabrasTotalesNoComunes;
+	@FXML
+	private Label labelRelevanciasAcertadas;
+	@FXML
+	private Spinner<IntegerSpinnerValueFactory> spinnerPin;
+	@FXML
+	private Button buttonProcesar;
+	@FXML
+	private Slider sliderIntervalos;
+	@FXML
+	private Button buttonGenerarF;
+	@FXML
+	private ProgressBar cargaF;
+	@FXML
+	private Button buttonVerFuncion;
+	@FXML
+	private Slider sliderSensibilidad;
+	@FXML
+	private Button buttonSimular;
+	@FXML
+	private ProgressBar cargaS;
+	@FXML
+	private Button buttonVerResultadoS;
+	@FXML
+	private Label labelRelAcertadas;
 	
 	private HashMap<String, ResultadoDataSet> mapDataSets;
+	
 
 	
 	@Override
@@ -133,6 +168,272 @@ public class VentanaController implements Initializable{
 	 */
 	public String[] getDataSets() {
 		return dataSets;
+	}
+
+	/**
+	 * @return the cargaLecturaDataSet
+	 */
+	public ProgressBar getCargaLecturaDataSet() {
+		return cargaLecturaDataSet;
+	}
+
+	/**
+	 * @param cargaLecturaDataSet the cargaLecturaDataSet to set
+	 */
+	public void setCargaLecturaDataSet(ProgressBar cargaLecturaDataSet) {
+		this.cargaLecturaDataSet = cargaLecturaDataSet;
+	}
+
+	/**
+	 * @return the buttonLeerDataSet
+	 */
+	public Button getButtonLeerDataSet() {
+		return buttonLeerDataSet;
+	}
+
+	/**
+	 * @param buttonLeerDataSet the buttonLeerDataSet to set
+	 */
+	public void setButtonLeerDataSet(Button buttonLeerDataSet) {
+		this.buttonLeerDataSet = buttonLeerDataSet;
+	}
+
+	/**
+	 * @return the labelCantidadDocumentos
+	 */
+	public Label getLabelCantidadDocumentos() {
+		return labelCantidadDocumentos;
+	}
+
+	/**
+	 * @param labelCantidadDocumentos the labelCantidadDocumentos to set
+	 */
+	public void setLabelCantidadDocumentos(Label labelCantidadDocumentos) {
+		this.labelCantidadDocumentos = labelCantidadDocumentos;
+	}
+
+	/**
+	 * @return the labelCantidadConsultas
+	 */
+	public Label getLabelCantidadConsultas() {
+		return labelCantidadConsultas;
+	}
+
+	/**
+	 * @param labelCantidadConsultas the labelCantidadConsultas to set
+	 */
+	public void setLabelCantidadConsultas(Label labelCantidadConsultas) {
+		this.labelCantidadConsultas = labelCantidadConsultas;
+	}
+
+	/**
+	 * @return the labelPalabrasTotalesComunes
+	 */
+	public Label getLabelPalabrasTotalesComunes() {
+		return labelPalabrasTotalesComunes;
+	}
+
+	/**
+	 * @param labelPalabrasTotalesComunes the labelPalabrasTotalesComunes to set
+	 */
+	public void setLabelPalabrasTotalesComunes(Label labelPalabrasTotalesComunes) {
+		this.labelPalabrasTotalesComunes = labelPalabrasTotalesComunes;
+	}
+
+	/**
+	 * @return the labelPalabrasTotalesNoComunes
+	 */
+	public Label getLabelPalabrasTotalesNoComunes() {
+		return labelPalabrasTotalesNoComunes;
+	}
+
+	/**
+	 * @param labelPalabrasTotalesNoComunes the labelPalabrasTotalesNoComunes to set
+	 */
+	public void setLabelPalabrasTotalesNoComunes(Label labelPalabrasTotalesNoComunes) {
+		this.labelPalabrasTotalesNoComunes = labelPalabrasTotalesNoComunes;
+	}
+
+	/**
+	 * @return the labelRelevanciasAcertadas
+	 */
+	public Label getLabelRelevanciasAcertadas() {
+		return labelRelevanciasAcertadas;
+	}
+
+	/**
+	 * @param labelRelevanciasAcertadas the labelRelevanciasAcertadas to set
+	 */
+	public void setLabelRelevanciasAcertadas(Label labelRelevanciasAcertadas) {
+		this.labelRelevanciasAcertadas = labelRelevanciasAcertadas;
+	}
+
+	/**
+	 * @return the spinnerPin
+	 */
+	public Spinner<IntegerSpinnerValueFactory> getSpinnerPin() {
+		return spinnerPin;
+	}
+
+	/**
+	 * @param spinnerPin the spinnerPin to set
+	 */
+	public void setSpinnerPin(Spinner<IntegerSpinnerValueFactory> spinnerPin) {
+		this.spinnerPin = spinnerPin;
+	}
+
+	/**
+	 * @return the buttonProcesar
+	 */
+	public Button getButtonProcesar() {
+		return buttonProcesar;
+	}
+
+	/**
+	 * @param buttonProcesar the buttonProcesar to set
+	 */
+	public void setButtonProcesar(Button buttonProcesar) {
+		this.buttonProcesar = buttonProcesar;
+	}
+
+	/**
+	 * @return the sliderIntervalos
+	 */
+	public Slider getSliderIntervalos() {
+		return sliderIntervalos;
+	}
+
+	/**
+	 * @param sliderIntervalos the sliderIntervalos to set
+	 */
+	public void setSliderIntervalos(Slider sliderIntervalos) {
+		this.sliderIntervalos = sliderIntervalos;
+	}
+
+	/**
+	 * @return the buttonGenerarF
+	 */
+	public Button getButtonGenerarF() {
+		return buttonGenerarF;
+	}
+
+	/**
+	 * @param buttonGenerarF the buttonGenerarF to set
+	 */
+	public void setButtonGenerarF(Button buttonGenerarF) {
+		this.buttonGenerarF = buttonGenerarF;
+	}
+
+	/**
+	 * @return the cargaF
+	 */
+	public ProgressBar getCargaF() {
+		return cargaF;
+	}
+
+	/**
+	 * @param cargaF the cargaF to set
+	 */
+	public void setCargaF(ProgressBar cargaF) {
+		this.cargaF = cargaF;
+	}
+
+	/**
+	 * @return the buttonVerFuncion
+	 */
+	public Button getButtonVerFuncion() {
+		return buttonVerFuncion;
+	}
+
+	/**
+	 * @param buttonVerFuncion the buttonVerFuncion to set
+	 */
+	public void setButtonVerFuncion(Button buttonVerFuncion) {
+		this.buttonVerFuncion = buttonVerFuncion;
+	}
+
+	/**
+	 * @return the sliderSensibilidad
+	 */
+	public Slider getSliderSensibilidad() {
+		return sliderSensibilidad;
+	}
+
+	/**
+	 * @param sliderSensibilidad the sliderSensibilidad to set
+	 */
+	public void setSliderSensibilidad(Slider sliderSensibilidad) {
+		this.sliderSensibilidad = sliderSensibilidad;
+	}
+
+	/**
+	 * @return the buttonSimular
+	 */
+	public Button getButtonSimular() {
+		return buttonSimular;
+	}
+
+	/**
+	 * @param buttonSimular the buttonSimular to set
+	 */
+	public void setButtonSimular(Button buttonSimular) {
+		this.buttonSimular = buttonSimular;
+	}
+
+	/**
+	 * @return the cargaS
+	 */
+	public ProgressBar getCargaS() {
+		return cargaS;
+	}
+
+	/**
+	 * @param cargaS the cargaS to set
+	 */
+	public void setCargaS(ProgressBar cargaS) {
+		this.cargaS = cargaS;
+	}
+
+	/**
+	 * @return the buttonVerResultadoS
+	 */
+	public Button getButtonVerResultadoS() {
+		return buttonVerResultadoS;
+	}
+
+	/**
+	 * @param buttonVerResultadoS the buttonVerResultadoS to set
+	 */
+	public void setButtonVerResultadoS(Button buttonVerResultadoS) {
+		this.buttonVerResultadoS = buttonVerResultadoS;
+	}
+
+	/**
+	 * @return the labelRelAcertadas
+	 */
+	public Label getLabelRelAcertadas() {
+		return labelRelAcertadas;
+	}
+
+	/**
+	 * @param labelRelAcertadas the labelRelAcertadas to set
+	 */
+	public void setLabelRelAcertadas(Label labelRelAcertadas) {
+		this.labelRelAcertadas = labelRelAcertadas;
+	}
+
+	/**
+	 * @return the mapDataSets
+	 */
+	public HashMap<String, ResultadoDataSet> getMapDataSets() {
+		return mapDataSets;
+	}
+
+	/**
+	 * @param mapDataSets the mapDataSets to set
+	 */
+	public void setMapDataSets(HashMap<String, ResultadoDataSet> mapDataSets) {
+		this.mapDataSets = mapDataSets;
 	}
 
 	
